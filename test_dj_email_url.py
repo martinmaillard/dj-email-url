martinmaillard/dj-email-url

commit abe128e7dd984cda54b67fa676622dbc92e78641

Allow URL encoded credentials

dj_email_url.py
@@ -56,8 +56,8 @@ def parse(url):
     # Update with environment configuration
     conf.update({
         'EMAIL_FILE_PATH': path,
-        'EMAIL_HOST_USER': url.username,
-        'EMAIL_HOST_PASSWORD': url.password,
+        'EMAIL_HOST_USER': urlparse.unquote(url.username),
+        'EMAIL_HOST_PASSWORD': urlparse.unquote(url.password),
         'EMAIL_HOST': url.hostname,
         'EMAIL_PORT': url.port,
     })

test_dj_email_url.py
@@ -49,6 +49,12 @@ class EmailTestSuite(unittest.TestCase):
         assert url['EMAIL_USE_SSL'] is False
         assert url['EMAIL_USE_TLS'] is True
 
+    def test_special_chars(self):
+        url = 'smtp://user%21%40%23%245678:pass%25%5E%26%2A%28%29123@' \
+              'smtp.example.com:587'
+        url = dj_email_url.parse(url)
+        assert url['EMAIL_HOST_PASSWORD'] == 'pass%^&*()123'
+        assert url['EMAIL_HOST_USER'] == 'user!@#$5678'
 
 if __name__ == '__main__':
     unittest.main()

commit 405fa3aa429c7f74cdb0dffc3703f4f0066844a8

Fix parsing URL without credentials
Broken in abe128e7d

dj_email_url.py
@@ -56,8 +56,8 @@ def parse(url):
     # Update with environment configuration
     conf.update({
         'EMAIL_FILE_PATH': path,
-        'EMAIL_HOST_USER': urlparse.unquote(url.username),
-        'EMAIL_HOST_PASSWORD': urlparse.unquote(url.password),
+        'EMAIL_HOST_USER': unquote(url.username),
+        'EMAIL_HOST_PASSWORD': unquote(url.password),
         'EMAIL_HOST': url.hostname,
         'EMAIL_PORT': url.port,
     })
@@ -82,3 +82,10 @@ def parse(url):
                 conf['EMAIL_USE_TLS'] = True
 
     return conf
+
+
+def unquote(value):
+    if value:
+        return urlparse.unquote(value)
+    else:
+        return value

test_dj_email_url.py
@@ -20,6 +20,18 @@ class EmailTestSuite(unittest.TestCase):
         assert url['EMAIL_PORT'] == 587
         assert url['EMAIL_USE_TLS'] is True
 
+    def test_console_parsing(self):
+        url = 'console://'
+        url = dj_email_url.parse(url)
+
+        assert url['EMAIL_BACKEND'] == \
+            'django.core.mail.backends.console.EmailBackend'
+        assert url['EMAIL_HOST'] is None
+        assert url['EMAIL_HOST_PASSWORD'] is None
+        assert url['EMAIL_HOST_USER'] is None
+        assert url['EMAIL_PORT'] is None
+        assert url['EMAIL_USE_TLS'] is False
+
     def test_email_url(self):
         a = dj_email_url.config()
         assert not a